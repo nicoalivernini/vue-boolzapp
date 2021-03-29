c5bae6bf03bc371b27a49dff2a168b437576cc53
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-29 15:26:36 +0200

    Creazione variabile con messaggio scritto dall'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ var app = new Vue ({
     data: {
       imgSrc: 'assets/img/avatar_',
       index: 0,
+      newMessage: '',
       contacts: [
       	{
       		name: 'Michele',
@@ -33,17 +34,17 @@ var app = new Vue ({
       		visible: true,
       		messages: [
       			{
-      				date: '03/20/2020 16:30:00',
+      				date: '20/03/2020 16:30:00',
       				text: 'Ciao come stai?',
       				status: 'sent'
       			},
       			{
-      				date: '03/20/2020 16:30:55',
+      				date: '20/03/2020 16:30:55',
       				text: 'Bene grazie! Stasera ci vediamo?',
       				status: 'received'
       			},
       			{
-      				date: '03/20/2020 16:35:00',
+      				date: '20/03/2020 16:35:00',
       				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
       				status: 'sent'
       			}
@@ -55,17 +56,17 @@ var app = new Vue ({
       		visible: true,
       		messages: [
       			{
-      				date: '03/28/2020 10:10:40',
+      				date: '28/03/2020 10:10:40',
       				text: 'La Marianna va in campagna',
       				status: 'received'
       			},
       			{
-      				date: '03/28/2020 10:20:10',
+      				date: '28/03/2020 10:20:10',
       				text: 'Sicuro di non aver sbagliato chat?',
       				status: 'sent'
       			},
       			{
-      				date: '03/28/2020 16:15:22',
+      				date: '28/03/2020 16:15:22',
       				text: 'Ah scusa!',
       				status: 'received'
       			}
@@ -104,6 +105,12 @@ var app = new Vue ({
         let ore = time.getHours();
         let minuti = time.getMinutes();
         return `${ore}:${minuti}`;
+      },
+
+      insertMessage: function () {
+        let newMessage = this.newMessage;
+        console.log(newMessage);
+        this.newMessage = '';
       }
 
     }, //Chiusura methods
